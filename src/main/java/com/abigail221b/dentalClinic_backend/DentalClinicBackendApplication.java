bb98f74ef2cf55663606e5d58762460d494da10c
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-20 18:20:01 -0800

    Initial commit

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/DentalClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abigail221b.dentalClinic_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalClinicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abigail221b/dentalClinic_backend/DentalClinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abigail221b.dentalClinic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
