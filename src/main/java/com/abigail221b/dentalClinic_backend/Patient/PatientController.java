b1cbcb17fdd5875f27121819fb06426cf894885d
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-31 15:30:34 -0800

    Initialize controller

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
@@ -0,0 +1,15 @@
+package com.abigail221b.dentalClinic_backend.Patient;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+
+
+}
