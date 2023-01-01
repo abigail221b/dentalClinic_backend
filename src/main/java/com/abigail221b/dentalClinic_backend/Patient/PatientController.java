a7206bc77acf2c0c89fb20fd2cd4819ea7bdecfd
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-31 19:14:23 -0800

    Handle Put request

--- src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,12 @@ public class PatientController {
     @PostMapping
     public ResponseEntity<Patient> savePatient(@RequestBody Patient patient) {
         return new ResponseEntity<>(patientService.save(patient), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable("id") Integer id, @RequestBody Patient patient) {
+        patient.setId(id);
+        return new ResponseEntity<>(patientService.update(patient), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
