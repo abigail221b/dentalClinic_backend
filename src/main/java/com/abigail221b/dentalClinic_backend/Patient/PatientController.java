993d79217ae05a2e24a844ffcfe9d0bb22e2ddfb
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-31 20:10:23 -0800

    Handle GET requests for patient search

--- src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
@@ -1,5 +1,6 @@
 package com.abigail221b.dentalClinic_backend.Patient;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,6 +32,78 @@ public class PatientController {
     @GetMapping("/{id}")
     public ResponseEntity<Patient> getPatientById(@PathVariable("id") Integer id) {
         return new ResponseEntity<>(patientService.findById(id).get(), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "firstName" }    
+    )
+    public ResponseEntity<List<Patient>> getPatientsByFirstName(@RequestParam("firstName") String firstName) {
+        return new ResponseEntity<>(patientService.findByFirstName(firstName), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "lastName" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByLastName(@RequestParam("lastName") String lastName) {
+        return new ResponseEntity<>(patientService.findByLastName(lastName), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "phoneNumber" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByPhoneNumber(@RequestParam("phoneNumber") String phoneNumber) {
+        return new ResponseEntity<>(patientService.findByPhoneNumber(phoneNumber), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "firstName", "lastName" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByFirstNameAndLastName(
+        @RequestParam("firstName") String firstName,
+        @RequestParam("lastName") String lastName
+    ) {
+        return new ResponseEntity<>(patientService.findByFirstNameAndLastName(firstName, lastName), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "firstName", "lastName", "dateOfBirth" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByFirstNameAndLastNameAndDateOfBirth(
+        @RequestParam("firstName") String firstName,
+        @RequestParam("lastName") String lastName,
+        @RequestParam("dateOfBirth") Date dateOfBirth
+    ) {
+        return new ResponseEntity<>(patientService.findByFirstNameAndLastNameAndDateOfBirth(firstName, lastName, dateOfBirth), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "firstName", "lastName", "phoneNumber" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByFirstNameAndLastNameAndDateOfBirth(
+        @RequestParam("firstName") String firstName,
+        @RequestParam("lastName") String lastName,
+        @RequestParam("phoneNumber") String phoneNumber
+    ) {
+        return new ResponseEntity<>(patientService.findByFirstNameAndLastNameAndPhoneNumber(firstName, lastName, phoneNumber), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        params = { "firstName", "lastName", "dateOfBirth", "phoneNumber" }
+    )
+    public ResponseEntity<List<Patient>> getPatientsByFirstNameAndLastNameAndDateOfBirth(
+        @RequestParam("firstName") String firstName,
+        @RequestParam("lastName") String lastName,
+        @RequestParam("dateOfBirth") Date dateOfBirth,
+        @RequestParam("phoneNumber") String phoneNumber
+    ) {
+        return new ResponseEntity<>(patientService.findByFirstNameAndLastNameAndDateOfBirthAndPhoneNumber(firstName, lastName, dateOfBirth, phoneNumber), HttpStatus.OK);
     }
 
     @PostMapping
