40b04b452acb5490ed83795651b350bcb00ce7f8
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-31 19:03:15 -0800

    Create PatientController CRUD methods

--- src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientController.java
@@ -1,6 +1,15 @@
 package com.abigail221b.dentalClinic_backend.Patient;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +19,26 @@ public class PatientController {
 
     @Autowired
     private PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients() {
+        return new ResponseEntity<>(patientService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(patientService.findById(id).get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> savePatient(@RequestBody Patient patient) {
+        return new ResponseEntity<>(patientService.save(patient), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable("id") Integer id) {
+        patientService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
