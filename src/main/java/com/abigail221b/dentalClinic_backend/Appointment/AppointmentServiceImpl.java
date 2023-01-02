84f34c3e6635e753ae23de8d67faa500a75a5b0c
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-02 15:20:14 -0800

    Inject AppointmentRepository

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentServiceImpl.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentServiceImpl.java
@@ -3,10 +3,14 @@ package com.abigail221b.dentalClinic_backend.Appointment;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentServiceImpl implements AppointmentService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
     @Override
     public List<Appointment> findAll() {
