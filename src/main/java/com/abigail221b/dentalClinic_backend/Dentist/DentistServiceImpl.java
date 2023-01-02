cac52ccb7df65fefb060da1e8c3e19e4f9d5490e
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-02 14:08:20 -0800

    Initialize Service implementation class

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Dentist/DentistServiceImpl.java
@@ -0,0 +1,41 @@
+package com.abigail221b.dentalClinic_backend.Dentist;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DentistServiceImpl implements DentistService {
+
+    @Override
+    public List<Dentist> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Dentist> findById(Integer id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Dentist save(Dentist dentist) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Dentist update(Dentist dentist) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
