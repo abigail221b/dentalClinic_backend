c4311797a4677c42e0ac97e69a86f38adb9f2601
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-02 15:18:39 -0800

    Initialize class to implement AppointmentService

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentServiceImpl.java
@@ -0,0 +1,41 @@
+package com.abigail221b.dentalClinic_backend.Appointment;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentServiceImpl implements AppointmentService {
+
+    @Override
+    public List<Appointment> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Appointment> findById(Integer id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Appointment save(Appointment Appointment) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Appointment update(Appointment Appointment) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
