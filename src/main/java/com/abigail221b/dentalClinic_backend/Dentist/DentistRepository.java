0ffc50b068051999585492c629509493c1229e3f
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-02 14:26:10 -0800

    Initialize Repository

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Dentist/DentistRepository.java
@@ -0,0 +1,5 @@
+package com.abigail221b.dentalClinic_backend.Dentist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DentistRepository extends JpaRepository<Dentist, Integer> {}
