9ca5d8a9ff097fd344963d0859c8fdc6433faddc
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 15:27:43 -0800

    Create DTOs for Appointment and AppointmentId

---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentDTO.java
@@ -0,0 +1,9 @@
+package com.abigail221b.dentalClinic_backend.Appointment;
+
+public class AppointmentDTO {
+    
+    private AppointmentIdDTO id;
+    private int dentistId;
+    private int duration;
+
+}
---
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentIdDTO.java
@@ -0,0 +1,12 @@
+package com.abigail221b.dentalClinic_backend.Appointment;
+
+import java.sql.Date;
+import java.time.LocalTime;
+
+public class AppointmentIdDTO {
+
+    private int patientId;
+    private Date date;
+    private LocalTime startTime;
+    
+}
