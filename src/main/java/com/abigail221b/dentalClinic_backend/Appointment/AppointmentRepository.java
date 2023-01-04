481deb68fe6a2db054e1a2e4233c6f79351a526c
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 19:57:52 -0800

    Add query method to find Appointments btwn dates

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package com.abigail221b.dentalClinic_backend.Appointment;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AppointmentRepository extends JpaRepository<Appointment, AppointmentId> {
 
     List<Appointment> findById_PatientId(Integer id);
+
+    List<Appointment> findById_DateBetween(Date after, Date before);
 
 }
