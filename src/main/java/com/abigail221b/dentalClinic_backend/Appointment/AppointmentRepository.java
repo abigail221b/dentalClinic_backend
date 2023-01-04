549923668ec51f811a62334f35ab1a67cb6a41e4
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 19:22:03 -0800

    Add method signature to find Appts by patientId

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
@@ -1,5 +1,11 @@
 package com.abigail221b.dentalClinic_backend.Appointment;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository extends JpaRepository<Appointment, AppointmentId> {}
+public interface AppointmentRepository extends JpaRepository<Appointment, AppointmentId> {
+
+    List<Appointment> findById_PatientId(Integer id);
+
+}
