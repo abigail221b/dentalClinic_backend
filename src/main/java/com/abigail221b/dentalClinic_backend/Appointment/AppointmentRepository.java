7b1504ca4142bb7403bfb377ed84fd7d90417b57
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 20:14:21 -0800

    Add method query

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
@@ -9,6 +9,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Appoin
 
     List<Appointment> findById_PatientId(Integer id);
 
+    List<Appointment> findByDentist_Id(Integer id);
+
     List<Appointment> findById_DateBetween(Date after, Date before);
 
 }
