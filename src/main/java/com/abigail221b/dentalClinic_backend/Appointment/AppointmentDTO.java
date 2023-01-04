0f2329622c5c53f2a19de8a9600cc681b0572f20
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 18:52:44 -0800

    Add constructors

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentDTO.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentDTO.java
@@ -1,8 +1,12 @@
 package com.abigail221b.dentalClinic_backend.Appointment;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentDTO {
     
     private AppointmentIdDTO id;
--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentIdDTO.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentIdDTO.java
@@ -3,9 +3,13 @@ package com.abigail221b.dentalClinic_backend.Appointment;
 import java.sql.Date;
 import java.time.LocalTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentIdDTO {
 
     private int patientId;
