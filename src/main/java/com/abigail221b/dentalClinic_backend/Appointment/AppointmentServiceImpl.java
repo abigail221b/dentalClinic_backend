6fc82039bfe38a46a02fa70cecfb4f1e2826c763
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-03 18:59:09 -0800

    Update implementations

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentServiceImpl.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentServiceImpl.java
@@ -6,11 +6,22 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.abigail221b.dentalClinic_backend.Dentist.Dentist;
+import com.abigail221b.dentalClinic_backend.Dentist.DentistRepository;
+import com.abigail221b.dentalClinic_backend.Patient.Patient;
+import com.abigail221b.dentalClinic_backend.Patient.PatientRepository;
+
 @Service
 public class AppointmentServiceImpl implements AppointmentService {
 
     @Autowired
     private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private DentistRepository dentistRepository;
 
     @Override
     public List<Appointment> findAll() {
@@ -18,23 +29,41 @@ public class AppointmentServiceImpl implements AppointmentService {
     }
 
     @Override
-    public Optional<Appointment> findById(AppointmentId id) {
-        return appointmentRepository.findById(id);
+    public Optional<Appointment> findById(AppointmentIdDTO id) {
+        Patient patient = patientRepository.findById(id.getPatientId()).get();
+        return appointmentRepository.findById(new AppointmentId(patient, id.getDate(), id.getStartTime()));
     }
 
     @Override
-    public Appointment save(Appointment appointment) {
-        return appointmentRepository.save(appointment);
+    public Appointment save(AppointmentDTO appointmentDTO) {
+        AppointmentIdDTO id = appointmentDTO.getId();
+        Patient patient = patientRepository.findById(id.getPatientId()).get();
+        Dentist dentist = dentistRepository.findById(appointmentDTO.getDentistId()).get();
+        return appointmentRepository.save(
+            new Appointment(
+                new AppointmentId(patient, id.getDate(), id.getStartTime()),
+                dentist,
+                appointmentDTO.getDuration()
+            ));
     }
 
     @Override
-    public Appointment update(Appointment appointment) {
-        return appointmentRepository.save(appointment);
+    public Appointment update(AppointmentDTO appointmentDTO) {
+        AppointmentIdDTO id = appointmentDTO.getId();
+        Patient patient = patientRepository.findById(id.getPatientId()).get();
+        Dentist dentist = dentistRepository.findById(appointmentDTO.getDentistId()).get();
+        return appointmentRepository.save(
+            new Appointment(
+                new AppointmentId(patient, id.getDate(), id.getStartTime()),
+                dentist,
+                appointmentDTO.getDuration()
+            ));
     }
 
     @Override
-    public void delete(AppointmentId id) {
-        appointmentRepository.deleteById(id);
+    public void delete(AppointmentIdDTO id) {
+        Patient patient = patientRepository.findById(id.getPatientId()).get();
+        appointmentRepository.deleteById(new AppointmentId(patient, id.getDate(), id.getStartTime()));
     }
     
 }
