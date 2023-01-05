81c9e32993c2d2743eb2703a4d11e5bf4686fe94
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-05 13:11:42 -0800

    Declare custom SQL query on repository method

--- src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientRepository.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Patient/PatientRepository.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
@@ -19,6 +20,14 @@ public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
     List<Patient> findByFirstNameAndLastNameAndPhoneNumber(String firstName, String lastName, String phoneNumber);
 
+    @Query(
+        value = "SELECT * FROM patient " +
+                "WHERE (?1 IS NULL OR first_name = ?1) " +
+                "AND (?2 IS NULL OR last_name = ?2) " +
+                "AND (?3 IS NULL OR date_of_birth = ?3) " +
+                "AND (?4 IS NULL OR phone_number = ?4);",
+        nativeQuery = true
+    )
     List<Patient> findBySearchParams(String firstName, String lastName, Date dateOfBirth, String phoneNumber);
 
 }
