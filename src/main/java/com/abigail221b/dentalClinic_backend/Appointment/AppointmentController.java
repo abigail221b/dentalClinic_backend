bb7547971ab7c6b8430feec3f305e23eb86538e0
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-11 09:47:50 -0800

    Add CrossOrigin annotation

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentController.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.MatrixVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/appointments")
 public class AppointmentController {
     
