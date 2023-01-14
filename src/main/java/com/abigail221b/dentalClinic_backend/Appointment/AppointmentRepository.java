53b84b542a29b5a5b52f909b0a060009a737443a
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-13 16:08:45 -0800

    Replace AppointmentId with Integer

--- src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
+++ src/main/java/com/abigail221b/dentalClinic_backend/Appointment/AppointmentRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository extends JpaRepository<Appointment, AppointmentId> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 
     List<Appointment> findById_PatientId(Integer id);
 
